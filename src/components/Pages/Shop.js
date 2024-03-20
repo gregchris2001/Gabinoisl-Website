1e6f94bfbcb840ae2e9f60a11c332b07b82b6b35
Uju Somto <officialdustman@gmail.com>
2024-03-19 22:19:50 -0700

    created the filterProducts function in shop.js

--- src/components/Pages/Shop.js
+++ src/components/Pages/Shop.js
@@ -1,15 +1,27 @@
 import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 import ProductContext from "../../store/product-context";
-
 
 const Shop = () => {
     const [shopResults, setShopResults] = useState([]);
     const location = useLocation();
     const { value } = location.state
-
     const { category } = useParams();
 
     const { productData } = useContext(ProductContext);
+
+    useEffect(() => {
+        filterProducts();
+    }, [productData, category, value]);
+
+    const filterProducts = () => {
+        if (productData && category && value) {
+            const filteredProducts = productData.filter(product => product[value].toLowerCase() === category.toLowerCase());
+            setShopResults(filteredProducts);
+        }
+    };
+    
+    console.log(shopResults)
 
     return (
         <>
